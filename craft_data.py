1c6c25bbd46b0dff2474f40bdf9c84f117400094
diewland <diewland@gmail.com>
2024-06-17 17:03:02 +0700

    add craft script

---
+++ craft_data.py
@@ -0,0 +1,28 @@
+# 1 pts / 1 vote, 6 votes = 6 pts
+'./src/snapshot_org.csv'
+
+# n nft = n pts, no cap
+'./src/Golden BLOBz.csv'
+'./src/Silver BLOBz.csv'
+
+# pts between 1 - 5 base on leaderboard points
+'./src/rar/Galxe leaderboard.csv'
+
+# max 5 pts per wallet
+'./src/rar/BLOBz x FWX minter_1pt.csv'
+
+# pts from filename
+'./src/rar/300 Bitcoin Bingo Free Entry_1 pt.csv'
+'./src/rar/BLOBz-QuestN follower_5 pt.csv'
+'./src/rar/Gull Network_1 pt.csv'
+'./src/rar/3,960 $JK Giveaway_1 pt.csv'
+'./src/rar/6,000 $SOMDEJ Giveaway_1 pt.csv'
+'./src/rar/1,000 $LQDX Giveaway_1 pt.csv'
+'./src/rar/100 $OP Giveaway_1 pt.csv'
+'./src/rar/Last_Chance_to_Mint_BLOBz!_1 pt.csv'
+'./src/rar/BLOBz_Invasion__2 pt.csv'
+'./src/rar/Get_Golden_BLOBz_1 pt.csv'
+'./src/rar/BLOBz_FWX_Trading_Contest._1 pt.csv'
+
+# play full quest ~ mid score of blobz-bt
+# google sheet for check points
